e58b1b739de52ac24b0ab7748f193fb17545768e
shariyerShazan <shariyershazan1@gmail.com>
2025-07-30 18:21:58 +0600

    menu model schema add

---
+++ models/menu.model.ts
@@ -0,0 +1,34 @@
+import mongoose, { Document } from "mongoose";
+
+export interface IMenu {
+    foodName: string ,
+    description: string ,
+    price: number ,
+    foodImage : string
+}
+
+export interface IMenuDocumnet extends IMenu , Document {
+    createdAt: Date ,
+    updatedAt: Date
+}
+
+const menuSchema = new  mongoose.Schema<IMenuDocumnet>({
+    foodName: {
+        type: String ,
+        required: true
+    },
+    description: {
+        type: String ,
+        required: true
+    },
+    price: {
+        type: Number ,
+        required: true
+    },
+    foodImage : {
+        type: String ,
+        required: true
+    }
+}, {timestamps: true})
+
+export const Menu = mongoose.model("Menu" , menuSchema)
