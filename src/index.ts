e1102e068fdd39ec429c75749a484cd31beb50bd
shariyerShazan <shariyershazan1@gmail.com>
2025-09-19 15:56:21 +0600

    code after deploy

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: [
         "http://localhost:5174",
-        "http://localhost:5173"
+        "http://localhost:5173" ,
+        "https://mealmart-client.vercel.app"
     ],
     credentials: true
 }));
